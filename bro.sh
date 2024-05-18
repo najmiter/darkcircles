6710e19be28361efdd7248c3d101b337d455c3f4
najmiter <realnajmiter@gmail.com>
2024-05-18 14:25:52 +0500

    modal styles fixes and a few other styles as well

--- bro.sh
+++ bro.sh
@@ -1,6 +1,8 @@
 if [ "$1" == "prod" ]; then
-    echo "Copying the latest script..."
+    echo "Minifying and copying the latest files..."
     minify script/koochi-koochi.js > prod/koochi-koochi.js
+    minify css/reset.css > prod/reset.css
+
 
     echo "Zipping the directory..."
     zip prod.zip prod/*
@@ -9,15 +11,19 @@ elif [ "$1" == "buildprod" ]; then
     bash bro.sh
     bash bro.sh prod
 
-elif [ "$1" == "push" ] && [ "$2" != "" ]; then
-    echo "Adding the commit..."
-    git add .
+elif [ "$1" == "push" ]; then
+    if [ "$2" == "" ]; then
+        echo "Please give a push message..."
+    else
+        echo "Adding the commit..."
+        git add .
 
-    echo "Commiting with the message '$2'..."
-    git commit -m "$2"
+        echo "Commiting with the message '$2'..."
+        git commit -m "$2"
 
-    echo "Pushing..."
-    git push
+        echo "Pushing..."
+        git push
+    fi
 
 else
     echo Building...
